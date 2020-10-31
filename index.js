9a140c2f5f15939b86d839769c21989de089220c
qwalley <willalley345@gmail.com>
2020-10-31 17:44:37 -0400

    guildMemberUpdate handler will send DM to users when they receive a role listed in config.json

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+// const bot = new Discord.Client({ ws: { intents: ['GUILD_PRESENCES', 'GUILD_MEMBERS'] } });
 const bot = new Discord.Client();
 const config = require('./config.json');
 bot.commands = new Discord.Collection();
@@ -12,23 +13,26 @@ Object.keys(botCommands).map(key => {
 const TOKEN = config.token;
 bot.login(TOKEN);
 
+const sendToDebug = async (content) => {
+	let channel = null;
+	try {
+		channel = await bot.channels.fetch(config.debug_channel);
+	} catch (error) {
+		throw "Error: Could not fetch debug_channel: " + ('\n' + error).replace(/\n/g, '\n\t');
+	}
+	try {
+		return await channel.send(content);
+	} catch (error) {
+		throw "Error: Could not send welcome message to debug channel: " + ('\n' + error).replace(/\n/g, '\n\t');
+	}
+}
+
 bot.on('ready', async () => {
   console.info(`Logged in as ${bot.user.tag}!`);
   if (!config.debug_channel) {
   	return;
   }
-  // fetch channel using ID from config
-  try {
-    channel = await bot.channels.fetch(config.debug_channel);
-  } catch (error) {
-    throw "Error: Could not fetch debug_channel: " + ('\n' + error).replace(/\n/g, '\n\t');
-  }
-  // send welcome message to debug channel
-  try {
-    return await channel.send(`Hello, my name is ${config.name}!`);
-  } catch (error) {
-    throw "Error: Could not send welcome message to debug channel: " + ('\n' + error).replace(/\n/g, '\n\t');
-  }
+  sendToDebug(`Hello, my name is ${config.name}!`).catch(error => console.error(error));
 });
 
 
@@ -36,7 +40,7 @@ bot.on('message', async msg => {
   // if master channel is configured
   if (config.debug_channel) {
     // check that message is on the correct channel 
-    if (config.config_channel !== msg.channel.id) return;
+    if (config.debug_channel !== msg.channel.id) return;
   }
   // assume first word is intended as a command
   const args = msg.content.split(/ +/);
@@ -56,4 +60,26 @@ bot.on('message', async msg => {
     console.error(error);
     msg.reply('there was an error trying to execute that command!');
   }
+});
+
+bot.on('guildMemberUpdate', async (oldMember, newMember) => {
+	
+	// only look at role changes
+	if (oldMember._roles.length >= newMember._roles.length) return;
+	
+	// check if each important role has been added
+	for (const role of config.on_role_change) {
+		
+		if (!oldMember._roles.includes(role.id)) {
+			
+			if (newMember._roles.includes(role.id)) {
+				try {
+					return await newMember.send(role.message);
+				} catch (error) {
+  					console.error(error);
+  					return sendToDebug(`Unable to DM ${newMember.user.username}!`).catch(error => console.error(error));
+				}
+			}
+		}
+	}
 });
